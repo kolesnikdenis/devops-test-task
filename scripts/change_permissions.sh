f0853a3d24c37b56ed2277a83baef6ec7dff267f
root <root@ip-172-31-24-210.eu-north-1.compute.internal>
2022-02-11 11:36:37 +0000

    change buildspec

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -3,6 +3,8 @@
 echo " `date` : change_permissions.sh "
 ls -ls /home/ubuntu/
 echo "/home/ubuntu/scripts/change_permissions.sh"
- aws ecr list-images     --repository-name test --region  eu-north-1
+
+aws ecr list-images     --repository-name test --region  eu-north-1
+docker service  update  test
 
 ) | tee -a /home/ubuntu/scripts/change_permissions.log
