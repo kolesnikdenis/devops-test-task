5bfe19694e9c515fb5e1c34b5b4509d7a9eca9eb
root <root@ip-172-31-24-210.eu-north-1.compute.internal>
2022-02-11 02:35:34 +0000

    disable build load docker images

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 (
 pwd
-cd /home/ubuntu/autobuild/ && \
-	rm -rf scripts  appspec.yml README.md && \
-	docker build . -t autobuild_test && \
-	docker image tag  autobuild_test localhost:5000/devops-test-task &&\
-	docker push localhost:5000/devops-test-task && \
-	docker rmi autobuild_test && \
-	docker service rm devops-test-task
+#cd /home/ubuntu/autobuild/ && \
+#	rm -rf scripts  appspec.yml README.md && \
+#	docker build . -t autobuild_test && \
+#	docker image tag  autobuild_test localhost:5000/devops-test-task &&\
+#	docker push localhost:5000/devops-test-task && \
+#	docker rmi autobuild_test && \
+#	docker service rm devops-test-task
 
 ) | tee -a /home/ubuntu/scripts/install_dependencies.log
 
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,7 +2,7 @@
 (
 echo " 
 `date` : stop_server.sh"
-docker service rm test  && \
-docker rmi localhost:5000/devops-test-task
+#docker service rm test  && \
+#docker rmi localhost:5000/devops-test-task
 
 ) | tee -a /home/ubuntu/scripts/stop_server.log
