054beca311db4fa4a246fd6d842532e3f3ff752d
root <root@ip-172-31-24-210.eu-north-1.compute.internal>
2022-02-13 02:25:14 +0000

    del Deploy appspec.yml

--- scripts/create_test_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-(
-echo " `date` : create_test_db.sh"
-ls -ls /home/ubuntu/
-echo "/home/ubuntu/scripts/create_test_db.sh"
-aws ecr list-images     --repository-name test --region  eu-north-1 
-) | tee -a /home/ubuntu/scripts/create_test_db.log
-
-
--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-(
-echo " 
-`date` : stop_server.sh"
-#docker service rm test  && \
-#docker rmi localhost:5000/devops-test-task
- aws ecr list-images     --repository-name test --region  eu-north-1
-
-) | tee -a /home/ubuntu/scripts/stop_server.log
